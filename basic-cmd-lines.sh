f77187998c61a3477eaa6e5692db2888ef07c7ac
wizardy <wizardhassan99@gmail.com>
2024-04-23 14:24:42 +0100

    commit

---
+++ basic-cmd-lines.sh
@@ -0,0 +1,10 @@
+command [OPTIONS] arguments #general syntax
+
+date #Displays the current date
+
+pwd #Displays the present working directory.
+
+ls #Lists the contents of the current directory.
+
+echo #Prints a string of text, or value of a variable to the terminal.
+
